11bc0e1cfc78c7393e866ab624520b1044f118c1
NIcholas Kneafsey <nicholaskneafsey@gmail.com>
2017-10-11 22:59:50 -0700

    Added services

--- server/schema/types/service_type.js
+++ server/schema/types/service_type.js
@@ -8,7 +8,21 @@ const ServiceType = new GraphQLEnumType({
     S3: { value: 'S3' },
     RDS: { value: 'RDS' },
     SQS: { value: 'SQS' },
-    DYNAMO_DB: { value: 'DYNAMO_DB' }
+    DYNAMO_DB: { value: 'DYNAMO_DB' },
+    LAMBDA: { value: 'LAMBDA' },
+    VPC: { value: 'VPC' },
+    CLOUDFRONT: { value: 'CLOUDFRONT' },
+    ROUTE_53: { value: 'ROUTE_53' },
+    GLACIER: { value: 'GLACIER' },
+    KINESIS: { value: 'KINESIS' },
+    STORAGE_GATEWAY: { value: 'STORAGE_GATEWAY' },
+    ECS: { value: 'ECS' },
+    ELASTICACHE: { value: 'ELASTICACHE' },
+    IAM: { value: 'IAM' },
+    SNS: { value: 'SNS' },
+    SES: { value: 'SES' },
+    STEP_FUNCTIONS: { value: 'STEP_FUNCTIONS' },
+    API_GATEWAY: { value: 'API_GATEWAY' }
   }
 })
 
