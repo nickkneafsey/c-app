c149fb54662f6c5a4fc8a26d5b6dfb339cf3447e
NIcholas Kneafsey <nicholaskneafsey@gmail.com>
2017-11-15 00:30:49 -0800

    Continuously try to reconnect to Mongo instance

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express()
 if (!MONGO_URI) { throw new Error('No mlab uri') }
 
 mongoose.Promise = global.Promise
-mongoose.connect(MONGO_URI, { server: { reconnectTries: 1 } }) // Number.MAX_VALUE will continuously retry
+mongoose.connect(MONGO_URI, { server: { reconnectTries: Number.MAX_VALUE } }) // Number.MAX_VALUE will continuously retry
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error))
